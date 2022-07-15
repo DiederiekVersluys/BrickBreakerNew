febf675718c3f189dfc1600a8a7650b233abde43
diederiek <didi_versluys@hotmail.com>
2022-07-15 14:35:37 +0200

    brickbreaker

---
+++ src/Gameplay.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks = 21;
+    private Timer timer;
+    private int delay = 8;
+    private int playerX = 310;
+    private int ballPosX = 120;
+    private int ballPosY = 315;
+    private int ballXDir = -1;
+    private int ballYDir = -2;
+    private MapGenerator map;
+
+    public Gameplay(){
+        map = new MapGenerator(3,7);
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
+
+
